88be7f0d7f581e3166386ca4a30cc23ade3ce084
Øyvin Richardsen <ori@dips.no>
2022-09-15 13:41:01 +0200

    Fix WSL and Docker setup

--- ubuntu_wsl.sh
+++ ubuntu_wsl.sh
@@ -4,6 +4,9 @@ echo "cd /c/code" >> ~/.bashrc
 
 # mount drives at /c instead of /mnt/c (fix for docker on WSL)
 sudo cp wsl/wsl.conf /etc/wsl.conf
+sudo chmod 644 /etc/wsl.conf
+
+sudo rm /etc/resolv.conf
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -51,8 +54,7 @@ sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
 sudo mkdir -p /etc/apt/keyrings
-wget https://download.docker.com/linux/ubuntu/gpg -O /etc/apt/keyrings/docker.gpg
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
