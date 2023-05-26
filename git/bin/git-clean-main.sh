93f933d41f6c8492cbd1f911f34df1ea995a1814
Øyvin Richardsen <ori@dips.no>
2023-05-26 11:54:23 +0200

    Update Git clean commands

--- git/bin/git-clean-main.sh
+++ git/bin/git-clean-main.sh
@@ -2,7 +2,9 @@
 # Checkout main, pull from main, prune main to remove branches that are merged with main
 ################################################################################################
 
+git stash
 git checkout main
 git pull
 git remote prune origin
-git branch --merged | grep -v "\*" | xargs -rn 1 git branch -d
+git branch --merged main --no-color | grep -v '^[ *]*main$' | xargs -rn 1 git branch -d
+git stash pop
